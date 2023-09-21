802ae8b12ee33d493569fd1609512da5c00ad0f5
Adi Miftahul Falah <145597403+AdiMiftahulFalah01@users.noreply.github.com>
2023-09-21 10:37:37 +0700

    Add files via upload

---
+++ Adi2/src/adi2/Adi2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adi2;
+
+/**
+ *
+ * @author Labkom-M12
+ */
+import java.util.Scanner;
+public class Adi2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama ;
+        int usia ;
+        double tinggi ;
+        
+        nama = "Adi Miftahul Falah";
+        usia = 22;
+        tinggi = 166;
+        
+        System. out. println("Nama: " + nama) ;
+        System. out. println("Usia: " + usia + "tahun") ;
+        System. out. println("Tinggi: " + tinggi + "cm") ;
+        
+    }
+    
+}
---
+++ Adi3/src/adi3/Adi3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adi3;
+
+/**
+ *
+ * @author Labkom-M12
+ */
+import java.util.Scanner;
+public class Adi3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 8, b = 4;
+        
+        int jumlah = a=b;
+        int kurang = a-b;
+        int kali = a*b;
+        int bagi = a/b;
+        System. out. println("penjumlahan :" + jumlah);
+        System. out. println("pengurangan :" + kurang);
+        System. out. println("perkalian :" + kali);
+        System. out. println("pembagian :" + bagi);
+        
+    }
+    
+}
